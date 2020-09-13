0ce1f2fc43cc0fe2b289b48e66a4590df9dd43d5
ElenaBadWolf <plastmassovaya@gmail.com>
2020-09-13 14:13:43 +0300

    add time sleep

--- test_items.py
+++ test_items.py
@@ -1,5 +1,9 @@
+import time
+
+
 def test_items(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
     button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert button, "there is no button"
 
